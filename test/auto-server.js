8aa5fd4189bc1f80f6c1e9294c62a0fa834fb33b
Stoyan Mishkovski <mishkovski@gmail.com>
2015-11-07 12:36:20 -0500

    Add new function for defining routes

--- test/auto-server.js
+++ test/auto-server.js
@@ -3,6 +3,16 @@ var expect = require('chai').expect;
 var http = require('http');
 
 describe('auto-server', function() {
+  var requestOptions = {
+    host: 'localhost',
+    port: 7777,
+    path: '/',
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+
   it('exists', function() {
     expect(autoServer).to.not.equal(undefined);
   });
@@ -10,26 +20,29 @@ describe('auto-server', function() {
   it('exports module functions', function() {
     expect(autoServer.setup).to.not.equal(undefined);
     expect(autoServer.start).to.not.equal(undefined);
-    expect(autoServer.addEndpoint).to.not.equal(undefined);
   });
 
   it('on start listens', function(done) {
     autoServer.start({}, function() {
-      var options = {
-        host: 'localhost',
-        port: 7777,
-        path: '/',
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      };
-
-      var req = http.request(options, function(response) {
+      var req = http.request(requestOptions, function(response) {
         expect(response.statusCode).to.equal(200);
         done();
       }).end();
 
+    });
+  });
+
+  it('defines a route', function(){
+    var testRoute = '/routeOne';
+
+    autoServer.define(testRoute, function(){
+      autoServer.start({}, function() {
+        requestOptions.path = testRoute;
+        var req = http.request(requestOptions, function(response) {
+          expect(response.statusCode).to.equal(200);
+          done();
+        }).end();
+      });
     });
   });
 });
